4087c1bd391ed3ada98c362a10b49f2331219335
cheng <vincentwu.b@gmail.com>
2026-06-24 08:32:57 +0800

    Lession 4

---
+++ src/robot_description/launch/gazebo.launch.py
@@ -0,0 +1,56 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory("robot_description")
+
+    xacro_file = os.path.join(
+        pkg_path,
+        "urdf",
+        "simple_robot.urdf.xacro"
+    )
+
+    world_file = os.path.join(
+        pkg_path,
+        "worlds",
+        "empty.sdf"
+    )
+
+    robot_description = Command([
+        "xacro ",
+        xacro_file
+    ])
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-r', world_file],
+            output="screen"
+        ),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{
+                "use_sim_time": True,
+                "robot_description": robot_description
+            }],
+            output="screen"
+        ),
+
+        Node(
+            package="ros_gz_sim",
+            executable="create",
+            arguments=[
+                "-topic", "robot_description",
+                "-name", "simple_robot",
+                "-x", "0",
+                "-y", "0",
+                "-z", "0.0"
+            ],
+            output="screen"
+        )
+    ])
